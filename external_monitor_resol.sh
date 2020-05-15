65ada72b706515dbf972cfbae98e5952a3f24c65
XCore64 <23143014+XCore64@users.noreply.github.com>
2020-05-15 11:00:41 -0700

    Update external_monitor_resol.sh

--- external_monitor_resol.sh
+++ external_monitor_resol.sh
@@ -1,2 +1,4 @@
+# Add your custom res based on the answer you got from get-new-resolution.sh
 xrandr --newmode "1360x768_60.00"   84.75  1360 1432 1568 1776  768 771 781 798 -hsync +vsync
+# Note: HDMI-1-2 is an example input. Change input accordingly
 xrandr --addmode HDMI-1-2 "1360x768_60.00"
